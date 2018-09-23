f7449133e4bf32076ada552fed9359f7422c4808
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2018-09-24 01:48:54 +0200

    Generation of playing board works

--- src/Board.py
+++ src/Board.py
@@ -1,4 +1,5 @@
 import random
+import numpy
 
 # A class describing the game board
 class Board:
@@ -32,59 +33,79 @@ class Board:
                 bomb_row.append(bomb)
             self.bomb_matrix.append(bomb_row)
 
+        self.print_bomb()
+
         # Calculates the board
-        self.board_matrix = self.bomb_matrix.copy()
+        self.board_matrix = []
         for row in range(self.h):
+            board_row = []
             for column in range(self.w):
-                if self.board_matrix[row][column] != 9:
-                    neighboring_bombs = 0
+                current_cell = self.bomb_matrix[row][column]
+                print(current_cell)
+                if current_cell != 9:
+                    lookup = get_neighbors(self.bomb_matrix, row, column)
+                    neighboring_bombs = count_neighbor_bombs(row, column, self.bomb_matrix, lookup)
+                    appendix = neighboring_bombs
+                else:
+                    appendix = 9
+                print("appendix: ", appendix)
+                board_row.append(appendix)
+            self.board_matrix.append(board_row)
+            self.print_board()
 
-
-    def print(self):
+    def print_bomb(self):
 
         print("\nBomb Matrix:\n")
         for row in self.bomb_matrix:
             print(row)
 
+    def print_board(self):
+
+        BOMB_SYMBOL = "X"
+
         print("\nBoard Matrix:\n")
-        for row in self.board_matrix:
-            print(row)
+        for r in self.board_matrix:
+            print(r)
 
 
+# Returns a lookup matrix for neighboring cells.
 def get_neighbors(matrix, x, y):
     h = len(matrix) - 1
     w = len(matrix[0]) - 1
+
+    print()
+    print("hw: ", h, w)
     lookup = []
 
     if x == 0 and y == 0:
         lookup = [[0, 0, 0],
                   [0, 1, 1],
                   [0, 1, 1]]
-    elif x != 0 != w and y == 0:
+    elif x == 0 and y != 0 and y != w:
         lookup = [[0, 0, 0],
                   [1, 1, 1],
                   [1, 1, 1]]
-    elif x == w and y == 0:
+    elif x == 0 and y == w:
         lookup = [[0, 0, 0],
                   [1, 1, 0],
                   [1, 1, 0]]
-    elif x == w and y != 0 != h:
+    elif x != 0 and x != h and y == w:
         lookup = [[1, 1, 0],
                   [1, 1, 0],
                   [1, 1, 0]]
-    elif x == w and y == h:
+    elif x == h and y == w:
         lookup = [[1, 1, 0],
                   [1, 1, 0],
                   [0, 0, 0]]
-    elif x != 0 != w and y == h:
+    elif x == h and y != 0 and y != w:
         lookup = [[1, 1, 1],
                   [1, 1, 1],
                   [0, 0, 0]]
-    elif x == 0 and y == h:
+    elif x == h and y == 0:
         lookup = [[0, 1, 1],
                   [0, 1, 1],
                   [0, 0, 0]]
-    elif x == 0 and y != 0 != h:
+    elif x != 0 and x != h and y == 0:
         lookup = [[0, 1, 1],
                   [0, 1, 1],
                   [0, 1, 1]]
@@ -93,9 +114,41 @@ def get_neighbors(matrix, x, y):
                   [1, 1, 1],
                   [1, 1, 1]]
 
+    for row in lookup:
+        print(row)
+
+    return lookup
+
+
+# Counts the neighbors which are bombs
+def count_neighbor_bombs(x, y, matrix, lookup):
+    bombs = 0
+    """
+    coordinates = [[[-1, -1],[0, -1],[1, -1]],
+                   [[-1, 0],[0, 0],[1, 0]],
+                   [[-1, 1],[0, 1],[1, 1]]]
+    """
+    coordinates = [[[-1, -1], [-1, 0], [-1, 1]],
+                   [[0, -1], [0, 0], [0, 1]],
+                   [[1, -1], [1, 0], [1, 1]],]
+    print("x y: ", x, y)
+    print("matrix_dim: ", len(matrix), len(matrix[0]))
+    for row in range(3):
+        for column in range(3):
+            if lookup[row][column] == 1:
+                look = coordinates[row][column]
+                x_p = x + look[0]
+                y_p = y + look[1]
+                print("look: ", look)
+                print("look: ", x_p, y_p)
+                if matrix[x_p][y_p] == 9:
+                    bombs += 1
+
+    print("bombs: ", bombs)
+
+    return bombs
 
 
 if __name__ == "__main__":
     board = Board(10, 9, 10)
     board.place_bombs()
-    board.print()
