7ae37c787a1901e656b5692850ab71b9e73b6ab0
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2020-02-02 22:29:58 +0100

    Early mockup done

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -15,7 +15,9 @@ def main():
     # root.resizable(width=False, height=False)
 
     # ----- Fonts -----
-    title_font = font.Font(family="Courier", size=21, weight="normal")
+    title_font = font.Font(size=21, weight="normal")
+    fit_font = font.Font(size=12, weight="normal")
+    nofit_font = font.Font(size=12, weight="normal")
     # ----- /Fonts -----
 
     # ----- Title -----
@@ -50,7 +52,7 @@ def main():
     bombs_separator_end = ttk.Separator(root, orient=HORIZONTAL)
 
     bombs_checkbutton_var = IntVar()
-    bombs_radiobutton1 = Radiobutton(root, text="Standard", variable=bombs_checkbutton_var, value=1)
+    bombs_radiobutton1 = Radiobutton(root, text="Standard (X % ratio)", variable=bombs_checkbutton_var, value=1)
     bombs_radiobutton2 = Radiobutton(root, text="Number", variable=bombs_checkbutton_var, value=2)
     bombs_radiobutton3 = Radiobutton(root, text="Ratio", variable=bombs_checkbutton_var, value=3)
 
@@ -64,6 +66,15 @@ def main():
     tilesize_label = Label(root, textvariable = tilesize_var)
 
     tilesize_separator_end = ttk.Separator(root, orient=HORIZONTAL)
+
+    tilesize_checkbutton_var = IntVar()
+    tilesize_radiobutton1 = Radiobutton(root, text="64 x 64 (large)", variable=tilesize_checkbutton_var, value=1)
+    tilesize_radiobutton2 = Radiobutton(root, text="32 x 32 (normal)", variable=tilesize_checkbutton_var, value=2)
+    tilesize_radiobutton3 = Radiobutton(root, text="16 x 16 (small)", variable=tilesize_checkbutton_var, value=3)
+    tilesize_radiobutton4 = Radiobutton(root, text="8 x 8 (ant)", variable=tilesize_checkbutton_var, value=4)
+    tilesize_radiobutton5 = Radiobutton(root, text="4 x 4 (binary)", variable=tilesize_checkbutton_var, value=5)
+    tilesize_radiobutton6 = Radiobutton(root, text="1 x 1 (pixel)", variable=tilesize_checkbutton_var, value=6)
+
     # ----- /Tilesize submenu -----
 
     # ----- Fitsscreen submenu -----
@@ -74,12 +85,20 @@ def main():
     fitsscreen_label = Label(root, textvariable = fitsscreen_var)
 
     fitsscreen_separator_end = ttk.Separator(root, orient=HORIZONTAL)
+
+    fit_var = StringVar()
+    fit_var.set("YES, the board fits the screen!")
+    fit_label = Label(root, textvariable = fit_var, font=fit_font)
+
+    nofit_var = StringVar()
+    nofit_var.set("NO, the board does NOT fit the screen!")
+    nofit_label = Label(root, textvariable = nofit_var, font=nofit_font)
     # ----- /Fitsscreen submenu -----
 
     # ----- Newgame submenu -----
     newgame_separator = ttk.Separator(root, orient=HORIZONTAL)
 
-    newgame_button = Button(root, text ="New Game", command = newgame_callback)
+    newgame_button = Button(root, text ="New Game", command = newgame_callback, font=title_font)
     newgame_separator_end = ttk.Separator(root, orient=HORIZONTAL)
     # ----- /Newgame submenu -----
 
@@ -112,12 +131,22 @@ def main():
     tilesize_label.pack()
     tilesize_separator_end.pack(fill="x")
 
+    tilesize_radiobutton1.pack(anchor="w")
+    tilesize_radiobutton2.pack(anchor="w")
+    tilesize_radiobutton3.pack(anchor="w")
+    tilesize_radiobutton4.pack(anchor="w")
+    tilesize_radiobutton5.pack(anchor="w")
+    tilesize_radiobutton6.pack(anchor="w")
+
     fitsscreen_separator.pack(fill="x")
     fitsscreen_label.pack()
     fitsscreen_separator_end.pack(fill="x")
 
+    fit_label.pack()
+    nofit_label.pack()
+
     newgame_separator.pack(fill="x")
-    newgame_button.pack()
+    newgame_button.pack(pady=8)
     newgame_separator_end.pack(fill="x")
 
     about_label.pack()
