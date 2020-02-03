afb60256414ff4f0bca28db085678029ae36b5bf
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2020-02-03 03:29:06 +0100

    Game can be launched both from init, and from launcher

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageTk
 def newgame_callback():
     print("New Game button clicked")
 
-    game.main()  # temporary solution
+    game.main(30, 16, 99, 64, False, [120, 72], [2560, 1440], 60, "InfiniSweeper")  # temporary solution
 
 def main():
     root = Tk()
