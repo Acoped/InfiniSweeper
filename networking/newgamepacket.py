c644e0f5136f678eb9e1caa6abc20fedfa3baa87
Andreas Kuoppa <andreas.kuoppa@gmail.com>
2020-02-07 08:50:51 +0100

    NewGamePackets serializer method works

--- networking/newgamepacket.py
+++ networking/newgamepacket.py
@@ -2,16 +2,39 @@ from game import Board
 
 
 class NewGamePacket:
-    """ A class representing new game (and restart) data to be sent or received for a multiplayer game over a network"""
+    """
+    A class representing new game (and restart) data to be sent or received for a multi-player game over a network
+    """
 
     def __init__(self, board: Board):
-        self.board = board
+        self.board: Board = board
 
     def __str__(self):
         self.board.print_board()
 
     def serialize(self) -> str:
-        return "0_"
+        serialized = "0_"
+        matrix = self.board.board_matrix
+        w = len(matrix[0])
+        h = len(matrix)
+
+        for x in range(w):
+            serialized += "/"
+            for y in range(h):
+                serialized += str(matrix[y][x])
+                if not y == h - 1:
+                    serialized += "_"
+
+        return serialized
 
     def deserializer(self, message: str):
         pass
+
+
+# Code for testing, mbe write unit tests?
+test_board = Board(5, 3, 3, 32)
+test_board.place_bombs()
+
+send_packet = NewGamePacket(test_board)
+# print(send_packet)
+print(send_packet.serialize())
